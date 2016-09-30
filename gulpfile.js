9e6a7c9e7679e94397d3110eae263b4a3d0872e7
a.palamarchuk <artem.palamarchuk.96@gmail.com>
2016-09-30 13:38:07 +0300

    gulp template

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp');
+var cleancss = require('gulp-clean-css');
+var browsersync = require('browser-sync');
+var rigger = require('gulp-rigger');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var rimraf = require('rimraf');
+var sequence = require('gulp-sequence');
+
+gulp.task('html', function() {
+	return gulp.src('src/index.html')
+			.pipe(rigger())
+			.pipe(gulp.dest('dist'))
+			.pipe(browsersync.reload({stream: true}))
+})
+
+gulp.task('css', function() {
+	return gulp.src('src/css/main.css')
+			.pipe(less())
+			.pipe(autoprefixer())
+			.pipe(cleancss())
+			.pipe(gulp.dest('dist/css'))
+			.pipe(browsersync.reload({stream: true}))
+})
+
+gulp.task('js', function() {
+	return gulp.src('src/js/common.js')
+			.pipe(rigger())
+			.pipe(uglify())
+			.pipe(gulp.dest('dist/js'))
+			.pipe(browsersync.reload({stream: true}))
+})
+
+gulp.task('img', function() {
+	return gulp.src('src/img/**/*.*')
+			.pipe(gulp.dest('dist/img'))
+			.pipe(browsersync.reload({stream: true}))
+})
+
+gulp.task('fonts', function() {
+	return gulp.src('src/fonts/**/*.*')
+			.pipe(gulp.dest('dist/fonts'))
+			.pipe(browsersync.reload({stream: true}))
+})
+
+gulp.task('watch', function(cb) {
+	sequence('html', 'css', 'js', ['img', 'fonts'], cb);
+	gulp.watch('src/**/*.html', ['html']);
+	gulp.watch('src/**/*.less', ['css']);
+	gulp.watch('src/**/*.js', ['js']);
+	gulp.watch('src/img/**/*.*', ['img']);
+	gulp.watch('src/fonts/**/*.*', ['fonts']);
+})
+
+gulp.task('default', sequence('cleandist', 'watch', 'browser'));
+
+gulp.task('browser', function() {
+    browsersync.init({
+        server: {
+            baseDir: "dist"
+        },
+        notify: false
+    });
+});
+
+gulp.task('cleandist', function(cb) {
+	rimraf('./dist', cb);
+})
